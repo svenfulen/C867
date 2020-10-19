c2b2aefe76f03081f6cd5ec373c6997f8dcced4b
svenfulen <sfulen99@gmail.com>
2020-10-18 21:29:34 -0500

    Improved notes

--- C867_3/degree.h
+++ C867_3/degree.h
@@ -2,5 +2,6 @@
 
 #include <string>
 
-//Rubric C. Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
+//Rubric C. 
+//Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
 enum DegreeProgram { SECURITY, NETWORK, SOFTWARE, DEF};
